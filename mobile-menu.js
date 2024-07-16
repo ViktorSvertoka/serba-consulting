8c09e9f63447208b2046ed30dd1bf4491310475c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-16 20:43:41 +0300

    Update mobile-menu.js

--- mobile-menu.js
+++ mobile-menu.js
@@ -9,34 +9,34 @@ const menuNavLinks = document.querySelectorAll('.menu__nav-link');
 function openMobileMenu() {
   mobileMenu.classList.add('is-open');
   backdrop.classList.remove('is-hidden');
-  document.body.classList.add('no-scroll'); // Додаємо клас, щоб запобігти скролінгу фону
+  document.body.classList.add('no-scroll');
 }
 
 // Функція для закриття мобільного меню
 function closeMobileMenu() {
   mobileMenu.classList.remove('is-open');
   backdrop.classList.add('is-hidden');
-  document.body.classList.remove('no-scroll'); // Видаляємо клас, дозволяючи скролінг тла
+  document.body.classList.remove('no-scroll');
 }
 
 // Обробник кліку на кнопку відкриття меню
-openMenuButton.addEventListener('click', () => {
-  openMobileMenu();
-});
+openMenuButton.addEventListener('click', openMobileMenu);
 
 // Обробник кліку на кнопку закриття меню
-closeMenuButton.addEventListener('click', () => {
-  closeMobileMenu();
-});
+closeMenuButton.addEventListener('click', closeMobileMenu);
 
 // Обробник кліка на бекдроп для закриття меню
-backdrop.addEventListener('click', () => {
-  closeMobileMenu();
+backdrop.addEventListener('click', e => {
+  // Закриваємо меню тільки при кліку на сам бекдроп
+  if (e.target === backdrop) {
+    closeMobileMenu();
+  }
 });
 
 // Обробник кліку на елементи меню для переходу до відповідної секції та закриття меню
 menuNavLinks.forEach(link => {
-  link.addEventListener('click', () => {
+  link.addEventListener('click', e => {
+    e.preventDefault(); // Зупиняємо стандартну поведінку посилання
     const targetId = link.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
 
