8ce39e22b20a6bcc586f6e6ffa232ddd26df3792
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-25 12:09:18 +0300

    Feat(HTML & JS) Added send form to mail

--- modal-window.js
+++ modal-window.js
@@ -3,13 +3,11 @@
   const closeModalBtn = document.querySelector('[data-modal-close]');
   const modal = document.querySelector('[data-modal]');
   const backdrop = document.querySelector('.js-backdrop');
-  const form = document.querySelector('form');
 
   openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
   closeModalBtn.addEventListener('click', toggleModal);
   backdrop.addEventListener('click', handleBackdropClick);
   window.addEventListener('keydown', handleKeyDown);
-  form.addEventListener('submit', handleFormSubmit);
 
   function toggleModal() {
     modal.classList.toggle('js-hidden');
@@ -26,11 +24,5 @@
     if (event.code === 'Escape' && !modal.classList.contains('js-hidden')) {
       toggleModal();
     }
-  }
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    console.log('Form submitted');
-    toggleModal();
   }
 })();
